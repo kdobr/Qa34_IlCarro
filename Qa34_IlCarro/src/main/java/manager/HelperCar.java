415e37e034094ba7a4c3e1d98c2b6471942ccc2e
TayaHatum <21taya21@gmail.com>
2022-07-06 21:04:23 +0300

    added car select

--- Qa34_IlCarro/src/main/java/manager/HelperCar.java
+++ Qa34_IlCarro/src/main/java/manager/HelperCar.java
@@ -2,7 +2,9 @@ package manager;
 
 
 import models.Car;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class HelperCar extends HelperBase{
     public HelperCar(WebDriver wd) {
@@ -13,7 +15,25 @@ public class HelperCar extends HelperBase{
     }
 
     public void fillCarForm(Car car){
+        typeAddress(car.getAddress());
 
+        select(By.id("fuel"),car.getFuel());
+
+    }
+
+    private void select(By locator, String option) {
+        Select select = new Select(wd.findElement(locator));
+
+        select.selectByValue(option);
+        //select.selectByIndex(1);
+       // select.deselectByVisibleText(option);
+    }
+
+    private void typeAddress(String address) {
+        type(By.id("pickUpPlace"),address);
+        click(By.cssSelector("div.pac-item"));
+        pause(500);
+        // click on first address
     }
 
     public void attachPhoto(String link) {
