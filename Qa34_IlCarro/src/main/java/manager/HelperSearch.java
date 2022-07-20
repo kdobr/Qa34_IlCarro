72cfbc35ca6fa00c2f52692aac476ee66fecba2b
TayaHatum <21taya21@gmail.com>
2022-07-20 20:57:39 +0300

    added search

--- Qa34_IlCarro/src/main/java/manager/HelperSearch.java
+++ Qa34_IlCarro/src/main/java/manager/HelperSearch.java
@@ -3,6 +3,9 @@ package manager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class HelperSearch extends HelperBase{
     public HelperSearch(WebDriver wd) {
         super(wd);
@@ -34,8 +37,9 @@ public class HelperSearch extends HelperBase{
 
     private void typeCity(String city) {
         type(By.id("city"),city);
-        click(By.cssSelector(".pac-item"));
         pause(500);
+        click(By.cssSelector(".pac-item"));
+
     }
 
     public void searchCurrentYear(String city, String dataFrom, String dataTo) {
@@ -45,6 +49,148 @@ public class HelperSearch extends HelperBase{
     }
 
     private void selectPeriodCurrentYear(String dataFrom, String dataTo) {
-        String now ="7/17/2022";
+        //     "8/10/2022", "10/20/2022"
+        String dataNow ="7/20/2022";
+       String[]now = dataNow.split("/");
+       String [] from = dataFrom.split("/");
+       String [] to = dataTo.split("/");
+        click(By.id("dates"));
+
+        if(!now[0].equals(from[0])){
+            int count =Integer.parseInt(from[0])-Integer.parseInt(now[0]);
+            clickByNextMonth(count);
+        }
+        String locator = String.format("//div[text()=' %s ']",from[1]);
+        click(By.xpath(locator));
+
+        if(Integer.parseInt(from[0])!=Integer.parseInt(to[0])) {
+            int count = Integer.parseInt(to[0])-Integer.parseInt(from[0]);
+            clickByNextMonth(count);
+        }
+
+        String locator1 = String.format("//div[text()=' %s ']",to[1]);
+        click(By.xpath(locator1));
+
     }
+
+
+    public void searchCurrentYearLocalDate(String city, String dataFrom, String dataTo) {
+        typeCity(city);
+        LocalDate now = LocalDate.now();
+        LocalDate from= LocalDate.parse(dataFrom, DateTimeFormatter.ofPattern("M/d/yyyy"));
+        LocalDate to = LocalDate.parse(dataTo,DateTimeFormatter.ofPattern("M/d/yyyy"));
+        click(By.id("dates"));
+        if(now.getMonthValue()!=from.getMonthValue()){
+            int count= from.getMonthValue()-now.getMonthValue();
+            clickByNextMonth(count);
+        }
+        String locator = String.format("//div[text()=' %s ']",from.getDayOfMonth());
+        click(By.xpath(locator));
+
+        if(from.getMonthValue()!= to.getMonthValue()){
+            int count=to.getMonthValue()-from.getMonthValue();
+            clickByNextMonth(count);
+        }
+        locator=String.format("//div[text()=' %s ']",to.getDayOfMonth());
+        click(By.xpath(locator));
+    }
+    public void searchAnyPeriodLocalDate2(String city, String dataFrom, String dataTo) {
+        typeCity(city);
+        LocalDate now = LocalDate.now();
+        LocalDate from= LocalDate.parse(dataFrom, DateTimeFormatter.ofPattern("M/d/yyyy"));
+        LocalDate to = LocalDate.parse(dataTo,DateTimeFormatter.ofPattern("M/d/yyyy"));
+        click(By.id("dates"));
+
+        int diffMonth = from.getYear()-now.getYear()
+                ==0 ? from.getMonthValue() -now.getMonthValue() : 12-now.getMonthValue()+ from.getMonthValue();
+
+        clickByNextMonth(diffMonth);
+        String locator = String.format("//div[text()=' %s ']",from.getDayOfMonth());
+        click(By.xpath(locator));
+
+        diffMonth= to.getYear()-from.getYear()
+                ==0  ?to.getMonthValue()-from.getMonthValue(): 12-from.getMonthValue()+to.getMonthValue();
+
+        clickByNextMonth(diffMonth);
+        locator=String.format("//div[text()=' %s ']",to.getDayOfMonth());
+        click(By.xpath(locator));
+    }
+
+
+    public void searchAnyPeriodLocalDate(String city, String dataFrom, String dataTo) {
+        typeCity(city);
+        LocalDate now = LocalDate.now();
+        LocalDate from= LocalDate.parse(dataFrom, DateTimeFormatter.ofPattern("M/d/yyyy"));
+        LocalDate to = LocalDate.parse(dataTo,DateTimeFormatter.ofPattern("M/d/yyyy"));
+        click(By.id("dates"));
+        // "8/10/2022", "3/20/2023"
+        int diffYear;
+        int diffMonth;
+
+        diffYear=from.getYear()-now.getYear();
+        if(diffYear==0){
+            diffMonth= from.getMonthValue()-now.getMonthValue(); /// 8-7 =1
+        }else {
+            diffMonth= 12-now.getMonthValue()+from.getMonthValue(); //12-7+3
+        }
+        clickByNextMonth(diffMonth);
+        String locator = String.format("//div[text()=' %s ']",from.getDayOfMonth());
+        click(By.xpath(locator));
+
+        ///**************
+        diffYear=to.getYear()-from.getYear();
+
+        if(diffYear==0){
+            diffMonth = to.getMonthValue()-from.getMonthValue();
+        }else {
+            diffMonth= 12-from.getMonthValue()+to.getMonthValue();
+        }
+        clickByNextMonth(diffMonth);
+        locator=String.format("//div[text()=' %s ']",to.getDayOfMonth());
+        click(By.xpath(locator));
+
+
+    }
+
+    private void clickByNextMonth(int count) {
+        for (int i = 0; i < count; i++) {
+            click(By.xpath("//button[@aria-label='Next month']"));
+        }
+    }
+
+    public boolean isListOfCarsAppeared() {
+        return isElementPresent(By.cssSelector(".cars-container.ng-star-inserted"));
+    }
+
+    public boolean isPageResultAppeared() {
+        String currentURL= wd.getCurrentUrl();
+        System.out.println(currentURL);
+        // https://ilcarro-1578153671498.web.app/search/results?city=Jerusalem&startDate=2022-07-21%2000:00&endDate=2022-07-22%2000:00
+        //currentURL.contains("results");
+        return currentURL.startsWith("https://ilcarro-1578153671498.web.app/search/results");
+    }
+
+    public void returnToHome() {
+        click(By.cssSelector(".logo"));
+    }
+
+    public void searchNextMonth(String city, String dataFrom, String dataTo) {
+
+        click(By.id("dates"));
+        click(By.xpath("//button[@aria-label='Next month']"));
+
+        String [] from = dataFrom.split("/");
+
+        String locator  = "//div[text()=' "+from[1]+" ']";
+        click(By.xpath(locator));
+
+        String [] to = dataTo.split("/");
+
+        String locator2 = String.format("//div[text()=' %s ']",to[1]);
+
+        click(By.xpath(locator2));
+    }
+
+
+
 }
--- Qa34_IlCarro/src/test/java/tests/SearchTests.java
+++ Qa34_IlCarro/src/test/java/tests/SearchTests.java
@@ -1,22 +1,64 @@
 package tests;
 
-import dev.failsafe.internal.util.Assert;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-public class SearchTests extends TestBase{
+public class SearchTests extends TestBase {
 
-@Test
-    public void searchCurrentMonth(){
-    app.search().searchCurrentMonth("Tel Aviv","7/28/2022","7/27/2022");
-    app.search().submit();
-   // Assert.isTrue(app.search().isListOfCarsAppeared());
+    @Test
+    public void searchCurrentMonth() {
+        app.search().searchCurrentMonth("Tel Aviv", "7/28/2022", "7/27/2022");
+        app.search().submit();
+        Assert.assertTrue(app.search().isListOfCarsAppeared());
 
-}
+    }
+    @Test
+    public void searchNextMonth() {
+        app.search().searchNextMonth("Tel Aviv", "8/28/2022", "8/27/2022");
+        app.search().submit();
+        Assert.assertTrue(app.search().isListOfCarsAppeared());
 
-@Test
-    public void searchCurrentYear(){
-    app.search().searchCurrentYear("Tel Aviv","8/10/2022","10/20/2022");
-    app.search().submit();
-    // Assert.isTrue(app.search().isListOfCarsAppeared());
-}
+    }
+
+
+    @Test
+    public void searchPeriodPast(){
+
+    }
+
+    @Test
+    public void searchCurrentYear() {
+        app.search().searchCurrentYear("Haifa", "8/10/2022", "8/20/2022");
+        app.search().submit();
+        Assert.assertTrue(app.search().isListOfCarsAppeared());
+        Assert.assertTrue(app.search().isPageResultAppeared());
+    }
+    @Test
+    public void searchCurrentYear2() {
+        app.search().searchCurrentYear("Haifa", "8/10/2022", "10/20/2022");
+        app.search().submit();
+        Assert.assertTrue(app.search().isListOfCarsAppeared());
+        Assert.assertTrue(app.search().isPageResultAppeared());
+    }
+
+    @Test
+    public void searchCurrentYearLocalDate(){
+        app.search().searchCurrentYearLocalDate("Haifa", "8/10/2022", "10/20/2022");
+        app.search().submit();
+        Assert.assertTrue(app.search().isListOfCarsAppeared());
+    }
+    @Test
+    public void searchAnyPeriod(){
+        app.search().searchAnyPeriodLocalDate2("Haifa", "6/10/2023", "7/15/2023");
+        app.search().submit();
+        Assert.assertTrue(app.search().isListOfCarsAppeared());
+    }
+
+
+    @AfterMethod
+    public void returnToHome(){
+        app.search().returnToHome();
+    }
 }
